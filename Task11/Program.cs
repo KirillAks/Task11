000a02dc2bbdecaa46eccf013db6995324088b56
Кирилл Аксенов <kirill.ax@tprofeng.ru>
2022-01-31 21:05:46 +0300

    Добавьте файлы проекта.

---
+++ Task11/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task11
+{
+    class Program
+    {
+        //Разработать структуру для решения линейного уравнения 0=kx+b. Коэффициенты уравнения k, b реализовать с помощью полей вещественного типа.
+        //Для решения уравнения предусмотреть метод Root. Создать экземпляр разработанной структуры. Осуществить использование экземпляра в программе.
+        static void Main(string[] args)
+        {
+            Console.Write("Введите коэфициент b=");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите коэфициент k=");
+            int k = Convert.ToInt32(Console.ReadLine());
+            StructureEquation structureEquation = new StructureEquation(b, k);
+            Console.Write("Решение уравнения равно {0}", structureEquation.Root());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task11/StructureEquation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task11
+{
+    struct StructureEquation
+    {
+        double b;
+        double k;        
+
+        public StructureEquation(double b, double k)
+        {
+            this.b = b;
+            this.k = k;
+        }
+        public double Root()
+        {
+            return -b / k;
+        }
+    }
+}
